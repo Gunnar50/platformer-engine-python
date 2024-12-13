94ca27496342c0109079a2ee0af738828d102607
gustavo <gustavo.passarella@hotmail.com>
2024-12-13 11:08:06 +0000

    Add debbuger

---
+++ src/shared/debug.py
@@ -0,0 +1,13 @@
+class LOGGER:
+
+  @staticmethod
+  def info(message):
+    print(f'INFO: {message}')
+
+  @staticmethod
+  def warning(message):
+    print(f'WARNING: {message}')
+
+  @staticmethod
+  def error(message):
+    print(f'ERROR: {message}')
