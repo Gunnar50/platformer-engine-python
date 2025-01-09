6a61fa468a5d41e717beab64b28459848d5ba5ab
gustavo <gustavo.passarella@hotmail.com>
2025-01-09 10:04:42 +0000

    Add serialisers

---
+++ src/shared/serialisers.py
@@ -0,0 +1,21 @@
+import abc
+
+
+class Serialiser(abc.ABC):
+
+  @abc.abstractmethod
+  def export(self, writer) -> None:
+    pass
+
+
+class NoExport(Serialiser):
+
+  def export(self, writer) -> None:
+    pass
+
+
+class Exportable(abc.ABC):
+
+  @abc.abstractmethod
+  def get_serialiser(self) -> Serialiser:
+    return NoExport()
