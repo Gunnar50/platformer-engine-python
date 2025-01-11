aaf0babbcacb4d905cdd3855ce1d99fc554a59ba
gustavo <gustavo.passarella@hotmail.com>
2025-01-11 10:19:57 +0000

    Add main file to load the game

---
+++ main.py
@@ -0,0 +1,36 @@
+from src.PlatformerGame.main.configs.build_config import BuildConfig
+from src.PlatformerGame.main.game_manager import GameManager
+from src.PyEng.main.engine import Engine
+
+
+class EngineTester:
+
+  def __init__(self) -> None:
+    self.engine = Engine.create()
+
+  def update(self) -> None:
+    self.engine.update()
+
+  def run(self) -> None:
+    while True:
+      self.update()
+
+
+class GameApp:
+
+  def __init__(self) -> None:
+    self.engine = Engine.create(BuildConfig)
+    self.game_manager = GameManager(self.engine)
+
+  def run(self) -> None:
+    while True:
+      self.game_manager.update()
+      # Update engine
+      self.engine.update()
+    # self.game_manager.clean_up()
+
+
+if __name__ == "__main__":
+  # EngineTester().run()
+  # GameApp().run()
+  GameApp()
