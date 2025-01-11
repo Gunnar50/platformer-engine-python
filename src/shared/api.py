5d88ccf53dd94242ff9b54bbefa944fc4c33493c
gustavo <gustavo.passarella@hotmail.com>
2025-01-11 10:22:25 +0000

    Add api and render component

--- src/shared/api.py
+++ src/shared/api.py
@@ -1,5 +1,21 @@
 import enum
 
+import pydantic
+import pygame
+
+from src.shared.types import Coordinate
+
+
+class RenderObjects(pydantic.BaseModel):
+  image: pygame.Surface
+  position: Coordinate
+  destination_surface: pygame.Surface
+  layer: int
+
+  # This is to allow pygame.Surface to be passed as a type
+  model_config = pydantic.ConfigDict(arbitrary_types_allowed=True)
+
+
 class KeyMapping(enum.Enum):
   QUIT = 'quit'
   RIGHT = 'right'
