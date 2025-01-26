c4ba40caef4c1b0bc95ecb5eee2f42a78dfd259d
Gustavo <gustavo.passarella@hotmail.com>
2025-01-26 19:38:28 +0000

    Add input

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.PlatformerGame.main.configs.build_config import BuildConfig
 from src.PlatformerGame.main.game_manager import GameManager
 from src.PyEng.main.engine import Engine
+from src.shared import api
 
 
 class EngineTester:
@@ -27,10 +28,13 @@ class GameApp:
       self.game_manager.update()
       # Update engine
       self.engine.update()
+      if self.game_manager.components_manager.get_by_class('Input').pressed(
+          api.KeyMapping.UP):
+        print('UP')
     # self.game_manager.clean_up()
 
 
 if __name__ == "__main__":
   # EngineTester().run()
-  # GameApp().run()
-  GameApp()
+  GameApp().run()
+  # GameApp()
--- src/PyEng/components/input.py
+++ src/PyEng/components/input.py
@@ -7,6 +7,7 @@ import pygame
 
 from src.PyEng.components.components import SystemComponent
 from src.shared import api, io
+from src.shared.debug import LOGGER
 
 
 class InputState:
@@ -58,6 +59,9 @@ class Input(SystemComponent):
     return self.input[key].just_released if key in self.input else False
 
   def update(self):
+    for state in self.input.values():
+      state.update()
+
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         pygame.quit()
@@ -67,6 +71,8 @@ class Input(SystemComponent):
         if event.key == pygame.K_ESCAPE:
           pygame.quit()
           sys.exit()
+        elif event.key == pygame.K_SPACE:
+          LOGGER.info(self.input)
 
 
 class Keyboard:
