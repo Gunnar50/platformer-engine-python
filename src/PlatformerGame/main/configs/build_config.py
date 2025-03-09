bc8cc8fcbb0c029592f8bfd074cc0442a49b2d7f
Gustavo Passarella <gustavo.passarella@hotmail.com>
2025-03-09 13:35:57 +0000

    Create editor config

--- src/PlatformerGame/main/configs/build_config.py
+++ src/PlatformerGame/main/configs/build_config.py
@@ -8,6 +8,22 @@ class BuildConfig(GameConfig):
   title = f'Platform Engine - Version: {GameConfig.version}'
   fullscreen = 0
   cheats = True
+  is_editor = False
+
+  # World settings
+  map_width = 10
+  map_height = 10
+  tile_width = 16
+  tile_height = 16
+
+
+class EditorConfig(GameConfig):
+  """Level Editor Configs"""
+  window_width = 1280
+  window_height = 720
+  title = f'Level Editor - Version: {GameConfig.version}'
+  fullscreen = 0
+  is_editor = True
 
   # World settings
   map_width = 10
